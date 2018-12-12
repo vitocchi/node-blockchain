aa1c2d6bb1a6b35cca49e4e6d84f0664eaf98557
Kosuke Ito <ito-kosuke0502@g.ecc.u-tokyo.ac.jp>
2018-12-12 21:06:14 +0900

    replace to longer chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -28,4 +28,16 @@ describe('Blockchain', () => {
         bc2.chain[1].data = 'Not foo';
         expect(bc.isValidChain(bc2.chain)).toBe(false);
     })
+
+    it('replaces the chain with a valid chain', () => {
+        bc2.addBlock('foo')
+        bc.replaceChain(bc2.chain)
+        expect(bc.chain).toEqual(bc2.chain)
+    })
+
+    it('does not replace the chain with one of less than or equal length', () => {
+        bc.addBlock('foo')
+        bc.replaceChain(bc2.chain)
+        expect(bc.chain).not.toEqual(bc2.chain)
+    })
 })
