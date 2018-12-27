61d73d21022b4fa07cc9dc7670bf48c46e87f5c8
Kosuke Ito <ito-kosuke0502@g.ecc.u-tokyo.ac.jp>
2018-12-28 03:06:21 +0900

    broadcalst-transaction

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,9 +1,13 @@
 const Websocket = require('ws')
 const P2P_PORT = process.env.P2P_PORT || 5001
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
+const MESSAGE_TYPES = {
+    chain: 'CHAIN',
+    transaction: 'TRANSACTION'
+}
 
 class P2pServer {
-    constructor(blockchain) {
+    constructor(blockchain, transactionPool) {
         this.blockchain = blockchain
         this.sockets = []
     }
@@ -31,7 +35,10 @@ class P2pServer {
     }
 
     sendChain(socket) {
-        socket.send(JSON.stringify(this.blockchain.chain))
+        socket.send(JSON.stringify({
+            type: MESSAGE_TYPES.chain,
+            chain: this.blockchain.chain
+        }))
     }
 
     syncChains() {
@@ -40,10 +47,28 @@ class P2pServer {
         })
     }
 
+    sendTransaction(socket, transaction) {
+        socket.send(JSON.stringify({
+            type: MESSAGE_TYPES.transaction,
+            transaction
+        }))
+    }
+
+    broadcastTransaction(transaction) {
+        this.sockets.forEach(socket => this.sendTransaction(socket, transaction))
+    }
+
     messageHandler(socket) {
         socket.on('message', message => {
             const data = JSON.parse(message)
-            this.blockchain.replaceChain(data)
+            switch (data.type) {
+                case MESSAGE_TYPES.chain:
+                    this.blockchain.replaceChain(data.chain)
+                    break;
+                case MESSAGE_TYPES.transaction:
+                    this.transactionPool.updateOrAddTransaction(data.transaction)
+                    break;
+            }
         })
     }
 }
